0c9b14d08b556a55afec8ae644c77e76c089c6c4
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-14 00:45:22 +0800

    add top bar and image ui

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,5 +1,7 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -7,6 +9,9 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +20,13 @@ fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
         //TODO: Insert code here
+        TopAppBar()
+        Image(
+            painter = painterResource(id = dish.imageResource),
+            contentDescription = dish.description,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
     }
 }
 
