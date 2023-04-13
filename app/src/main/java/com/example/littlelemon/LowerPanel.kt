cf774bd9586050cf35d7982b865186da6f233ced
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-14 00:13:47 +0800

    Display the dish properties

--- app/src/main/java/com/example/littlelemon/LowerPanel.kt
+++ app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -1,7 +1,9 @@
 package com.example.littlelemon
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,8 +11,11 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -48,6 +53,27 @@ fun MenuDish(navController: NavHostController? = null, dish: Dish) {
         navController?.navigate(DishDetails.route + "/${dish.id}")
     }) {
         //TODO: Insert code here
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column() {
+                Text(
+                    text = dish.name, fontSize = 18.sp, fontWeight = Bold
+                )
+                Text(
+                    text = dish.description,
+                    modifier = Modifier
+                        .padding(top = 5.dp, bottom = 5.dp)
+                        .fillMaxWidth(.75f)
+                )
+                Text(
+                    text = "${dish.price}", fontSize = 18.sp, fontWeight = Bold
+                )
+            }
+            Image(painter = painterResource(id = dish.imageResource), contentDescription = dish.description)
+        }
     }
     Divider(
         modifier = Modifier.padding(start = 8.dp, end = 8.dp),
