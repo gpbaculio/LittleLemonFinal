baa82dde4c81f39d32b9134a1df3e7efb8d0f68d
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-14 01:20:30 +0800

    update ui from theme

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -3,10 +3,7 @@ package com.example.littlelemon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +16,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.littlelemon.ui.theme.LittleLemonColor
 
 @Composable
 fun DishDetails(id: Int) {
@@ -38,6 +36,10 @@ fun DishDetails(id: Int) {
             Counter()
             Button(
                 onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = LittleLemonColor.yellow,
+                    contentColor = LittleLemonColor.green
+                )
             ) {
                 Text(
                     text = "${stringResource(id = R.string.add_for)} ${dish.price}",
